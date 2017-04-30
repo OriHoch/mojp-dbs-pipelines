25aad28342dbd980c13f572a4e26b9ca781cd8f0
Ori Hoch <ori@uumpa.com>
2017-04-30 10:44:16 +0300

    support for jinja2 pipeline fixtures

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,5 +1,7 @@
-from datapackage_pipelines.utilities.lib_test_helpers import ProcessorFixtureTestsBase
+from datapackage_pipelines.utilities.lib_test_helpers import ProcessorFixtureTestsBase, rejsonize
 import os
+from jinja2 import Template
+import json
 
 ROOT_PATH = os.path.join(os.path.dirname(__file__), '..')
 
@@ -9,20 +11,35 @@ class MojpDbsPipelinesFixtureTests(ProcessorFixtureTestsBase):
     def _get_processor_file(self, processor):
         return os.path.join(ROOT_PATH, "mojp_dbs_pipelines", processor.strip())
 
-    def _replace_tokens(self, instr):
-        objs = [{"id": 1, "doc": "{\"title\": \"foobar\", \"content\": \"bazbax\"}"},
-                {"id": 2, "doc": "{\"title\": \"222\", \"content\": \"2222\"}"}]
-        objs_str = [str(obj) for obj in objs]
-        MOCK_DOWNLOADED_ROWS = "\n".join(objs_str)
-        instr = instr.decode() if hasattr(instr, "decode") else instr
-        outstr = instr.replace("--MOCK_DOWNLOADED_ROWS--",
-                               MOCK_DOWNLOADED_ROWS)
-        return outstr.encode("utf-8")
+    def get_tests(self):
+        for filename, func in super(MojpDbsPipelinesFixtureTests, self).get_tests():
+            yield filename.replace(".jinja2", ""), func
 
     def _load_fixture(self, dirpath, filename):
-        data_in, data_out, dp_out, params, processor_file = super(MojpDbsPipelinesFixtureTests, self)._load_fixture(dirpath, filename)
-        return self._replace_tokens(data_in), self._replace_tokens(data_out), self._replace_tokens(dp_out), params, processor_file
+        with open(os.path.join(dirpath, filename), encoding='utf8') as f:
+            template_file = f.read()
+        if filename.endswith(".jinja2"):
+            rendered_file = Template(template_file).render(self.get_context(filename))
+        else:
+            rendered_file = template_file
+        parts = rendered_file.split('--\n')
+        processor, params, dp_in, data_in, dp_out, data_out = parts
+        processor_file = self._get_processor_file(processor)
+        params = rejsonize(params)
+        dp_out = rejsonize(dp_out)
+        dp_in = rejsonize(dp_in)
+        data_in = (dp_in + '\n\n' + data_in).encode('utf8')
+        print(data_out.split("\n"))
+        return data_in, data_out, dp_out, params, processor_file
+
+    def get_context(self, filename):
+        context = {}
+        if filename == "clearmash_add_resources.jinja2":
+            mock_downloaded_rows = [json.dumps({"id": 1, "doc": json.dumps({"title": "foobar", "content": "bazbax"})}),
+                                    json.dumps({"id": 2, "doc": json.dumps({"title": "222", "content": "2222"})})]
+            context["MOCK_DOWNLOADED_ROWS"] = "\n".join(mock_downloaded_rows)
+        return context
 
 
-for filename, testfunc in MojpDbsPipelinesFixtureTests(os.path.join(os.path.dirname(__file__), 'fixtures')).get_tests():
-    globals()['test_fixture_%s' % filename] = testfunc
+for filename, func in MojpDbsPipelinesFixtureTests(os.path.join(os.path.dirname(__file__), 'fixtures')).get_tests():
+    globals()['test_fixture_%s' % filename] = func
