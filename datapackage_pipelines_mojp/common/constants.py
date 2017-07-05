9fbd0f56a4fc414f2cc9cf011493181678b0bc2d
Ori Hoch <ori@uumpa.com>
2017-07-05 12:48:35 +0300

    adds slugs with same logic as we had for BHP items, adds new slugs, never replaces existing slugs (#31)

--- datapackage_pipelines_mojp/common/constants.py
+++ datapackage_pipelines_mojp/common/constants.py
@@ -6,3 +6,11 @@ COLLECTION_PERSONALITIES = "personalities"
 COLLECTION_PHOTOUNITS = "photoUnits"
 ALL_KNOWN_COLLECTIONS = [COLLECTION_PLACES, COLLECTION_FAMILY_NAMES, COLLECTION_MOVIES,
                          COLLECTION_PERSONALITIES, COLLECTION_PHOTOUNITS]
+
+SLUG_LANGUAGES_MAP = {
+    COLLECTION_PLACES: {'en': 'place', 'he': u'מקום',},
+    COLLECTION_FAMILY_NAMES: {'en': 'familyname', 'he': u'שםמשפחה',},
+    COLLECTION_PHOTOUNITS: {'en': 'image', 'he': u'תמונה',},
+    COLLECTION_PERSONALITIES: {'en': 'luminary', 'he': u'אישיות',},
+    COLLECTION_MOVIES: {'en': 'video', 'he': u'וידאו',},
+}
