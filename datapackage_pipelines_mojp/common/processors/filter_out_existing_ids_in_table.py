e4f3e9f71240be4ba9eb4cf1a02a218cdb76c920
Ori Hoch <ori@uumpa.com>
2017-07-26 21:27:01 +0300

    fix memory problem with filter_out_existing_ids processor

--- datapackage_pipelines_mojp/common/processors/filter_out_existing_ids_in_table.py
+++ datapackage_pipelines_mojp/common/processors/filter_out_existing_ids_in_table.py
@@ -6,12 +6,15 @@ class Processor(BaseProcessor):
 
     def __init__(self, *args, **kwargs):
         super(Processor, self).__init__(*args, **kwargs)
-        db_table = self.db_meta.tables.get(self._parameters["table"])
-        if db_table is not None:
-            self._existing_ids = [int(row[getattr(db_table.c, self._parameters["id-column"])])
-                                  for row in db_table.select().execute().fetchall()]
+        self._db_table = self.db_meta.tables.get(self._parameters["table"])
+        if self._db_table is not None:
+            self._id_column = getattr(self._db_table.c, self._parameters["id-column"])
+            self._existing_ids = self._get_existing_ids()
         else:
             self._existing_ids = []
+
+    def _get_existing_ids(self):
+        return [int(row[0]) for row in self.db_session.query(self._id_column).all()]
 
     def _filter_row(self, row):
         if int(row[self._parameters["id-field"]]) in self._existing_ids:
