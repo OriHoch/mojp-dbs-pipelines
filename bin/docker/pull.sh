346058009d30d696088bbd2ac50318f299de359d
Ori Hoch <ori@uumpa.com>
2017-07-27 22:56:59 +0300

    fix docker pull

--- bin/docker/pull.sh
+++ bin/docker/pull.sh
@@ -4,8 +4,8 @@ set -e
 
 docker pull orihoch/mojp-dbs-back
 docker pull orihoch/mojp-pipelines-tests
-docker pull orihoch/mojp-dbs-tests
+docker pull orihoch/mojp-dbs-pipelines
 
 docker tag orihoch/mojp-dbs-back mojp/dbs-back
 docker tag orihoch/mojp-pipelines-tests mojp/pipelines-tests
-docker tag orihoch/mojp-dbs-tests mojp/pipelines-app
+docker tag orihoch/mojp-dbs-pipelines mojp/pipelines-app
