3a1875869962015bd2d8efa2438a2b58d9bcb996
Ori Hoch <ori@uumpa.com>
2017-07-27 07:58:32 +0300

    load sql resource - changed to load in batches to limit memory usage

--- datapackage_pipelines_mojp/common/processors/load_sql_resource.py
+++ datapackage_pipelines_mojp/common/processors/load_sql_resource.py
@@ -7,6 +7,7 @@ class LoadSqlResource(BaseProcessor):
 
     def __init__(self, parameters=None, datapackage=None, resources=None):
         super(LoadSqlResource, self).__init__(parameters, datapackage, resources)
+        self._db_table = self.db_meta.tables.get(self._parameters["load-table"])
 
     def _get_schema(self):
         with open(self._parameters["datapackage"]) as f:
@@ -15,18 +16,33 @@ class LoadSqlResource(BaseProcessor):
                     self._schema = resource["schema"]
                     return self._schema
 
+    def _get_id_column(self):
+        primary_keys = self._schema.get("primaryKey")
+        if primary_keys and len(primary_keys) == 1:
+            return getattr(self._db_table.c, primary_keys[0])
+        else:
+            raise Exception("failed to find an appropriate primary key in the schema")
+
+    def _get_base_query(self):
+        q = self.db_session.query(self._db_table).order_by(self._id_column)
+        where = self._parameters.get("where")
+        if where:
+            q = q.filter(text(where))
+        return q
+
     def _get_resource(self):
-        table = self.db_meta.tables.get(self._parameters["load-table"])
-        if table is not None:
-            q = table.select()
-            if self._parameters.get("where"):
-                q = q.where(text(self._parameters["where"]))
-            for db_row in q.execute().fetchall():
-                row = {}
-                for field in self._schema["fields"]:
-                    val = db_row[getattr(table.c, field["name"])]
-                    row[field["name"]] = val
-                yield row
+        if self._db_table is not None:
+            self._id_column = self._get_id_column()
+            num_total = self._get_base_query().count()
+            batch_size = 10
+            for i in range(0, num_total, batch_size):
+                for db_row in self._get_base_query().slice(i, i+batch_size).all():
+                    row = {}
+                    for field in self._schema["fields"]:
+                        val = getattr(db_row, field["name"])
+                        row[field["name"]] = val
+                    yield row
+                logging.info("loaded {} / {}".format(i, num_total))
 
 
 if __name__ == '__main__':
