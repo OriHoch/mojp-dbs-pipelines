2e045ece531a00af732f20dba1ee1405fac0a445
Ori Hoch <ori@uumpa.com>
2017-07-28 07:53:35 +0300

    kill the db session in filter out existing ids after init

--- datapackage_pipelines_mojp/common/processors/filter_out_existing_ids_in_table.py
+++ datapackage_pipelines_mojp/common/processors/filter_out_existing_ids_in_table.py
@@ -6,15 +6,16 @@ class Processor(BaseProcessor):
 
     def __init__(self, *args, **kwargs):
         super(Processor, self).__init__(*args, **kwargs)
+
+    def _process(self, datapackage, resources):
         self._db_table = self.db_meta.tables.get(self._parameters["table"])
         if self._db_table is not None:
             self._id_column = getattr(self._db_table.c, self._parameters["id-column"])
-            self._existing_ids = self._get_existing_ids()
+            self._existing_ids = [int(row[0]) for row in self.db_session.query(self._id_column).all()]
         else:
             self._existing_ids = []
-
-    def _get_existing_ids(self):
-        return [int(row[0]) for row in self.db_session.query(self._id_column).all()]
+        self.db_commit()
+        return super(Processor, self)._process(datapackage, resources)
 
     def _filter_row(self, row):
         if int(row[self._parameters["id-field"]]) in self._existing_ids:
