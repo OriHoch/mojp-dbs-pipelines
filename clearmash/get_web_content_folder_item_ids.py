d448ef987680eb65c5fe892b4d107140ec6b5b77
Ori Hoch <ori@uumpa.com>
2017-12-27 11:27:33 +0200

    updated pipeline for better handling of max recursion

--- clearmash/get_web_content_folder_item_ids.py
+++ clearmash/get_web_content_folder_item_ids.py
@@ -1,7 +1,6 @@
-import logging, sys, os
+import logging, sys, os, json
 from datapackage_pipelines.wrapper import ingest, spew
 from datapackage_pipelines.utilities.resources import PROP_STREAMING
-
 from datapackage_pipelines_mojp.clearmash.api import ClearmashApi
 from datapackage_pipelines_mojp.clearmash.constants import CONTENT_FOLDERS
 
@@ -18,7 +17,8 @@ else:
 
 
 parameters = dict({"max-recursion-depth": int(os.environ.get("MAX_RECURSION_DEPTH", "-1"))}, **parameters)
-aggregations = {"stats": {}}
+stats = {}
+aggregations = {"stats": stats}
 
 
 api = ClearmashApi()
@@ -27,17 +27,36 @@ api = ClearmashApi()
 def get_folder_items(folder_id, folder, recursion_depth=0):
     doc = api.get_web_document_system_folder(folder_id)
     for item in doc["Items"]:
-        yield {"folder_id": folder_id,
-               "collection": folder.get("collection"),
+        collection = folder.get("collection")
+        if not collection:
+            collection = "unknown"
+        row = {"folder_id": folder_id,
+               "collection": collection,
                "item_id": item["Id"],
                "item_name": item["Name"],
                "item_is_published": item["IsPublished"],
                "item_is_searchable": item["IsSearchable"],
                "item_permissions_type": item["PermissionType"], }
+        yield row
+        stats.setdefault(collection, {"total": 0, "published": 0, "searchable": 0, "permission_type_is_not_1": 0})
+        stats[collection]["total"] += 1
+        if row["item_is_published"]:
+            stats[row["collection"]]["published"] += 1
+        if row["item_is_searchable"]:
+            stats[row["collection"]]["searchable"] += 1
+        if row["item_permissions_type"] != 1:
+            stats[row["collection"]]["permission_type_is_not_1"] += 1
     for folder in doc["Folders"]:
         if parameters["max-recursion-depth"] == -1 or recursion_depth < parameters["max-recursion-depth"]:
             yield from get_folder_items(folder["Id"], {"collection": "unknown"}, recursion_depth+1)
         else:
+            yield {"folder_id": None,
+                   "collection": None,
+                   "item_id": None,
+                   "item_name": json.dumps({"reached max recursion depth": True, "folder_id": folder["Id"]}),
+                   "item_is_published": False,
+                   "item_is_searchable": False,
+                   "item_permissions_type": -1}
             logging.warning("Reached folders max recursion depth")
 
 
@@ -59,5 +78,5 @@ else:
                                                              {"name": "item_name", "type": "string"},
                                                              {"name": "item_is_published", "type": "boolean"},
                                                              {"name": "item_is_searchable", "type": "boolean"},
-                                                             {"name": "item_permissions_type", "type": "integer"},]}}]),
+                                                             {"name": "item_permissions_type", "type": "integer"},],}}]),
          [get_resource()], aggregations["stats"])
