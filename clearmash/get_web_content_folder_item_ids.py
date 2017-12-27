c2e02306e00ecf86de7fd0d121cf3ed0ffabfb83
Ori Hoch <ori@uumpa.com>
2017-12-27 14:49:50 +0200

    get all entities and folders

--- clearmash/get_web_content_folder_item_ids.py
+++ clearmash/get_web_content_folder_item_ids.py
@@ -2,7 +2,28 @@ import logging, sys, os, json
 from datapackage_pipelines.wrapper import ingest, spew
 from datapackage_pipelines.utilities.resources import PROP_STREAMING
 from datapackage_pipelines_mojp.clearmash.api import ClearmashApi
-from datapackage_pipelines_mojp.clearmash.constants import CONTENT_FOLDERS
+
+
+FIELDS = (("community_id", "CommunityId", "integer"),
+          ("creator_person_id", "CreatorPersonId", "integer"),
+          ("file_type", "FileType", "integer"),
+          ("id", "Id", "integer"),
+          ("is_bookmarked", "IsBookmarked", "boolean"),
+          ("is_folder", "IsFolder", "boolean"),
+          ("is_liked", "IsLiked", "boolean"),
+          ("is_published", "IsPublished", "boolean"),
+          ("is_readonly", "IsReadOnly", "boolean"),
+          ("is_searchable", "IsSearchable", "boolean"),
+          ("likes_count", "LikesCount", "integer"),
+          ("locked_by_person_id", "LockedByPersonId", "integer"),
+          ("locked_by_person_name", "LockedByPersonName", "string"),
+          ("modified_by_person_id", "ModifiedByPersonId", "integer"),
+          ("modified_by_person_name", "ModifiedByPersonName", "string"),
+          ("name", "Name", "string"),
+          ("parent_folder_id", "ParentFolderId", "integer"),
+          ("permission_type", "PermissionType", "integer"),
+          ("size_in_bytes", "SizeInBytes", "integer"),
+          ("user_can_publish", "UserCanPublish", "boolean"),)
 
 
 CLI_MODE = len(sys.argv) > 1 and sys.argv[1] == '--cli'
@@ -17,6 +38,7 @@ else:
 
 
 parameters = dict({"max-recursion-depth": int(os.environ.get("MAX_RECURSION_DEPTH", "-1"))}, **parameters)
+logging.info(parameters)
 stats = {}
 aggregations = {"stats": stats}
 
@@ -24,59 +46,52 @@ aggregations = {"stats": stats}
 api = ClearmashApi()
 
 
-def get_folder_items(folder_id, folder, recursion_depth=0):
+def get_folder_row(folder):
+    return {r: folder[o] for r, o, t in FIELDS}
+
+
+def get_row(source_row, parent_folder_names=None, parent_folder_ids=None):
+    row = {r: source_row[o] for r, o, t in FIELDS}
+    row.update(parent_folder_names=parent_folder_names, parent_folder_ids=parent_folder_ids)
+    return row
+
+
+def get_folder_items(folder_id, recursion_depth=0, recursion_folders=None):
+    recursion_folders = [] if not recursion_folders else recursion_folders
+    parent_folder_names, parent_folder_ids = None, None
+    if len(recursion_folders) > 0:
+        parent_folder_names = ">".join([folder["Name"] for folder in recursion_folders])
+        parent_folder_ids = ">".join([str(folder["Id"]) for folder in recursion_folders])
+    if recursion_depth > 2:
+        logging.info("reucrsion depth = {}, parent_folder_names = {}".format(recursion_depth, parent_folder_names))
     doc = api.get_web_document_system_folder(folder_id)
     for item in doc["Items"]:
-        collection = folder.get("collection")
-        if not collection:
-            collection = "unknown"
-        row = {"folder_id": folder_id,
-               "collection": collection,
-               "item_id": item["Id"],
-               "item_name": item["Name"],
-               "item_is_published": item["IsPublished"],
-               "item_is_searchable": item["IsSearchable"],
-               "item_permissions_type": item["PermissionType"], }
-        yield row
-        stats.setdefault(collection, {"total": 0, "published": 0, "searchable": 0, "permission_type_is_not_1": 0})
-        stats[collection]["total"] += 1
-        if row["item_is_published"]:
-            stats[row["collection"]]["published"] += 1
-        if row["item_is_searchable"]:
-            stats[row["collection"]]["searchable"] += 1
-        if row["item_permissions_type"] != 1:
-            stats[row["collection"]]["permission_type_is_not_1"] += 1
+        yield get_row(item, parent_folder_names, parent_folder_ids)
     for folder in doc["Folders"]:
+        yield get_row(folder, parent_folder_names, parent_folder_ids)
         if parameters["max-recursion-depth"] == -1 or recursion_depth < parameters["max-recursion-depth"]:
-            yield from get_folder_items(folder["Id"], {"collection": "unknown"}, recursion_depth+1)
+            recursion_folders.append(folder)
+            yield from get_folder_items(folder["Id"], recursion_depth+1, recursion_folders)
         else:
-            yield {"folder_id": None,
-                   "collection": None,
-                   "item_id": None,
-                   "item_name": json.dumps({"reached max recursion depth": True, "folder_id": folder["Id"]}),
-                   "item_is_published": False,
-                   "item_is_searchable": False,
-                   "item_permissions_type": -1}
             logging.warning("Reached folders max recursion depth")
 
 
 def get_resource():
-    for folder_id, folder in CONTENT_FOLDERS.items():
-        yield from get_folder_items(folder_id, folder)
+    for folder in api.get_documents_root_folders():
+        yield get_row(folder)
+        yield from get_folder_items(folder["Id"], 0, [folder])
 
 
 if CLI_MODE:
     for item in get_resource():
         print(item)
 else:
-    spew(dict(datapackage, resources=[{PROP_STREAMING: True,
-                                       "name": "entity-ids",
-                                       "path": "entity-ids.csv",
-                                       "schema": {"fields": [{"name": "folder_id", "type": "integer"},
-                                                             {"name": "collection", "type": "string"},
-                                                             {"name": "item_id", "type": "integer"},
-                                                             {"name": "item_name", "type": "string"},
-                                                             {"name": "item_is_published", "type": "boolean"},
-                                                             {"name": "item_is_searchable", "type": "boolean"},
-                                                             {"name": "item_permissions_type", "type": "integer"},],}}]),
+    entity_ids_resource = {PROP_STREAMING: True,
+                           "name": "entity-ids",
+                           "path": "entity-ids.csv",
+                           "schema": {"fields": [{"name": r, "type": t} for r, o, t in FIELDS],
+                                      "primaryKey": ["id"]}}
+    entity_ids_resource["schema"]["fields"] += [{"name": "parent_folder_names", "type": "string"},
+                                                {"name": "parent_folder_ids", "type": "string"}]
+    spew(dict(datapackage, resources=[entity_ids_resource]),
          [get_resource()], aggregations["stats"])
